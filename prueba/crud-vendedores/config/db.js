93430020cf979598e755a33eb80f073a955c257c
LUIS CAJACURI <luiscajacurigutierrezv@gmail.com>
2026-04-23 19:34:30 -0500

    sistema_ventas

--- prueba/crud-vendedores/config/db.js
+++ prueba/crud-vendedores/config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: "shinkansen.proxy.rlwy.net",
+  host: "localhost",
   user: "root",
-  password: "DoVmAnqxaiAVqyhwsRLSyALUdvRaQBCe",
-  database: "railway",
-  port: 44237,
+  password: "",
+  database: "sistema_ventas",
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
